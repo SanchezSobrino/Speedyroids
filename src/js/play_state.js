b9f657171b83f781b8966b4e95839dc417bf4212
Santiago Sánchez <sanchezsobrino@gmail.com>
2016-11-02 10:49:16 +0100

    Fixed ship rotation algorithm. Added some fancy particles.

--- src/js/play_state.js
+++ src/js/play_state.js
@@ -31,6 +31,7 @@ var PlayState = {
         this._physModule.apply_velocity(this._shipObject, 0.0, 0.0, 0.0);
         this._shipSpeedX = 10.0;
         this._shipSpeedY = 10.0;
+        this._mousePosition = this._vec3Module.create();
     },
 
     _initControls: function() {
@@ -38,11 +39,11 @@ var PlayState = {
             keyA = this._ctrlModule.create_keyboard_sensor(this._ctrlModule.KEY_A),
             keyS = this._ctrlModule.create_keyboard_sensor(this._ctrlModule.KEY_S),
             keyD = this._ctrlModule.create_keyboard_sensor(this._ctrlModule.KEY_D),
-            mouseMove = this._ctrlModule.create_mouse_move_sensor();
+            mouseSensor = this._ctrlModule.create_mouse_move_sensor();
+        var aimSensor = this._ctrlModule.create_elapsed_sensor();
 
-        var controlSensors = [keyW, keyA, keyS, keyD, mouseMove];
+        var controlSensors = [keyW, keyA, keyS, keyD, mouseSensor];
         var movementLogic = function(s) { return s[0] || s[1] || s[2] || s[3]; };
-        var aimLogic = function(s) { return s[4]; };
 
         function controlFunc(obj, id, pulse) {
             var dirX = 0,
@@ -54,55 +55,52 @@ var PlayState = {
                     dirX = -this._ctrlModule.get_sensor_value(obj, id, 1);
                     dirY = !!this._ctrlModule.get_sensor_value(obj, id, 2) ? -1 : dirY;
                     dirX = !!this._ctrlModule.get_sensor_value(obj, id, 3) ? 1 : dirX;
+                } else if (id === 'MOUSE') {
+                    // TODO: cache create_* calls out from here
+                    // Get 2D screen coordinates
+                    var mousePosCanvas = this._ctrlModule.get_sensor_payload(obj, id, 4);
+
+                    // Emit ray from the camera
+                    var cam = this._scenesModule.get_active_camera();
+                    var pline = this._camModule.calc_ray(cam, mousePosCanvas[0], mousePosCanvas[1], this._mathModule.create_pline());
+                    var camera_ray = this._mathModule.get_pline_directional_vec(pline, this._vec3Module.create());
+
+                    // Calculate ray/floor plane intersection point
+                    var cam_trans = this._transModule.get_translation(cam, this._vec3Module.create());
+                    this._mathModule.set_pline_initial_point(pline, cam_trans);
+                    this._mathModule.set_pline_directional_vec(pline, camera_ray);
+                    this._mousePosition = this._mathModule.line_plane_intersect([0, 0, 1], 0, pline, this._vec3Module.create());
                 }
             }
-
-
-            // TODO: Optimize it
-            var objPos = this._transModule.get_translation(obj);
-            objPos[2] = 0.0;
-            var objPosNormalized = this._vec3Module.create();
-            this._vec3Module.normalize(objPos, objPosNormalized);
-
-            var mousePosCanvas = this._ctrlModule.get_sensor_payload(obj, id, 4);
-
-            // Emit ray from the camera
-            var cam = this._scenesModule.get_active_camera();
-            var plineTmp = this._mathModule.create_pline();
-            var pline = this._camModule.calc_ray(cam, mousePosCanvas[0], mousePosCanvas[1], plineTmp);
-            var vec3Tmp1 = this._vec3Module.create();
-            var camera_ray = this._mathModule.get_pline_directional_vec(pline, vec3Tmp1);
-
-            // Calculate ray/floor plane intersection point
-            var vec3Tmp2 = this._vec3Module.create();
-            var cam_trans = this._transModule.get_translation(cam, vec3Tmp2);
-            this._mathModule.set_pline_initial_point(pline, cam_trans);
-            this._mathModule.set_pline_directional_vec(pline, camera_ray);
-            var vec3Tmp = this._vec3Module.create();
-            var point = this._mathModule.line_plane_intersect([0, 0, 1], 0, pline, vec3Tmp);
-
-            var mousePosNormalized = this._vec3Module.create();
-            this._vec3Module.normalize(point, mousePosNormalized);
-
-            var angle = Math.acos(this._vec3Module.dot(objPosNormalized, mousePosNormalized));
-            console.log(angle * (180/3.1415926));
-            var rotationQuat = this._quatModule.create();
-            this._utilModule.euler_to_quat([0.0, 0.0, angle], rotationQuat);
-
-            var quatTmp = this._quatModule.create();
-            this._transModule.get_rotation(obj, quatTmp);
-            this._quatModule.multiply(quatTmp, rotationQuat, quatTmp);
-            this._transModule.set_rotation_v(obj, quatTmp);
 
             if (id === 'MOVEMENT') {
                 this._physModule.apply_force_world(obj, this._shipSpeedX * dirX, this._shipSpeedY * dirY, 0.0);
             }
         }
 
+        function aimFunc(obj, id, pulse) {
+            // TODO: cache create_* calls out from here
+            var objPos = this._transModule.get_translation(obj);
+            var direction = this._vec3Module.subtract(this._mousePosition, objPos, this._vec3Module.create());
+            var distance = this._vec3Module.length(direction);
+
+            var quatTmp = this._transModule.get_rotation(obj, this._quatModule.create());
+            var origin = this._utilModule.quat_to_dir(quatTmp, [0, 1, 0], this._vec3Module.create());
+            origin[2] = 0.0;
+            direction[2] = 0.0;
+            var originNormalized = this._vec3Module.normalize(origin, this._vec3Module.create())
+            var directionNormalized = this._vec3Module.normalize(direction, this._vec3Module.create());
+            var rotationQuat = this._quatModule.rotationTo(originNormalized, directionNormalized, this._quatModule.create());
+
+            this._transModule.set_rotation_v(obj, rotationQuat);
+        }
+
         this._ctrlModule.create_sensor_manifold(this._shipObject, 'MOVEMENT', this._ctrlModule.CT_CONTINUOUS,
                                                 controlSensors, movementLogic, controlFunc.bind(this));
+        this._ctrlModule.create_sensor_manifold(this._shipObject, 'MOUSE', this._ctrlModule.CT_CONTINUOUS,
+                                                controlSensors, function(s) { return s[4]; }, controlFunc.bind(this));
         this._ctrlModule.create_sensor_manifold(this._shipObject, 'AIM', this._ctrlModule.CT_CONTINUOUS,
-                                                controlSensors, aimLogic, controlFunc.bind(this));
+                                                [aimSensor], function(s) { return s[0]; }, aimFunc.bind(this));
     },
 
     exit: function() {
